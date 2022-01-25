6c12bdc9af830df17a840237fc12bde9233d1a71
Murillo C Leta <murillocasemiro@gmail.com>
2022-01-25 10:09:32 -0300

    Iniciação do projeto

---
+++ ByteBank.SistemaAgencia/ListaDeContaCorrente.cs
@@ -0,0 +1,15 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    // [item][item][item][item][item]
+    //                                ^
+    //                                 `- _proximaPosicao
+
+    public class ListaDeContaCorrente { }
+}
