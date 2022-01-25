f2006b32aad015c6fcbc7443f63cb77ada2cd5a7
Murillo C Leta <murillocasemiro@gmail.com>
2022-01-25 15:22:35 -0300

    create class:ListaContaCorrente

---
+++ ByteBank.SistemaAgencia/ListaContaCorrente.cs
@@ -0,0 +1,54 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class ListaContaCorrente
+    {
+        private ContaCorrente[] _itens;
+        private int _proximaPosicao;
+
+        public ListaContaCorrente(int capacidadeInicial = 6)
+        {
+            _itens = new ContaCorrente[capacidadeInicial];
+            _proximaPosicao = 0;
+        }
+
+        public void Adicionar(ContaCorrente item)
+        {
+            VerificarCapacidade(_itens.Length + 1);
+
+            _itens[_proximaPosicao] = item;
+            _proximaPosicao++;
+
+            Console.WriteLine(item + " " + _proximaPosicao);
+        }
+
+        private void VerificarCapacidade(int tamanhoNecessario)
+        {
+            if (_itens.Length >= tamanhoNecessario)
+            {
+                return;
+            }
+
+            int novoTamanho = _itens.Length * 2;
+            if (novoTamanho < tamanhoNecessario)
+            {
+                novoTamanho = tamanhoNecessario;
+            }
+
+            ContaCorrente[] novoArray = new ContaCorrente[novoTamanho];
+
+            for (int i = 0; i < _itens.Length; i++)
+            {
+                novoArray[i] = _itens[i];
+            }
+
+            _itens = novoArray;
+        }
+    }
+}
--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -12,9 +12,46 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-                      
+            ListaContaCorrente lista = new ListaContaCorrente();
+
+            lista.Adicionar(new ContaCorrente(123, 12345689));
+            lista.Adicionar(new ContaCorrente(123, 12345689));
+            lista.Adicionar(new ContaCorrente(123, 12345689));
 
             Console.ReadLine();
+        }
+
+        static void TestaArrayDeContaCorrente()
+        {
+            ContaCorrente[] contas = new ContaCorrente[]
+            {
+                new ContaCorrente(12, 123456789),
+                new ContaCorrente(12, 123456789),
+                new ContaCorrente(12, 123456789)
+            };
+
+            Console.WriteLine(contas);
+        }
+
+        static void TesteArrayInt()
+        {
+            int[] idades = new int[5];
+
+            idades[0] = 15;
+            idades[1] = 28;
+            idades[2] = 35;
+            idades[3] = 50;
+            idades[4] = 28;
+
+            int cont = 0;
+            int media = 0;
+            for (int i = 0; i <= 5; i++)
+            {
+                media += idades[i];
+                cont++;
+            }
+            
+            Console.WriteLine(media/cont);
         }
     }
 }
